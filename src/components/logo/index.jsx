988b03b918fb35e54f7ab10281bb48f9551d70f8
Aniket762 <aniketindian8@gmail.com>
2021-04-21 12:10:24 +0530

    initialized

---
+++ src/components/logo/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import orsiLogo from "../../assets/images/logo.png";
+
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LogoImg = styled.div`
+  width: 50px;
+  height: 50px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+
+export function Logo(props) {
+  return (
+    <LogoWrapper>
+      <LogoImg>
+        <img src={orsiLogo} alt="Greenland logo" />
+      </LogoImg>
+    </LogoWrapper>
+  );
+}
