595e9f8e1a87a056a29ac4231793cccf89c9cbb8
Aniket762 <aniketindian8@gmail.com>
2021-04-23 03:01:34 +0530

    landing page init

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-image: linear-gradient(to right, #6a11cb 0%, #2575fc 100%);
+    direction:'flex';
+    flex-direction:'row';
+    overflow:hidden;
+    
+`;
+
+export const Image = styled.img`
+    height:570px;
+    margin-top: 100px;
+    margin-left:650px;
+    position:absolute;
+    top:0;
+`;
+
+export const BackgroundImage = styled.img`
+    height:670px;
+    width:1935px;
+    margin-left:-200px;
+    margin-bottom:-20px;
+    
+
+`;
 
 function home() {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <Container>
+            <Image src={require('../assets/images/robot.png').default} />
+            <BackgroundImage src={require('../assets/images/home-bg.png').default} />
+        </Container>
     )
 }
 
