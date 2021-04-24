0c46a9b054ca1109682febc2e34a99a646fe1cd8
Aniket762 <aniketindian8@gmail.com>
2021-04-24 13:49:01 +0530

    map added

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -21,7 +21,8 @@ const contactUs = {
 function contact() {
     return (
         <div>
-        <InfoSection {...contactUs} />
+            <InfoSection {...contactUs} />
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117057.25839218969!2d87.23013350301412!3d23.531087979490394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f7710b47a89171%3A0x429e1bdb57e009dd!2sDurgapur%2C%20West%20Bengal!5e0!3m2!1sen!2sin!4v1619251923405!5m2!1sen!2sin"  style={{width:'100%',height:'500px', boxShadow:'2px 2px 15px #6C63FF'}} allowfullscreen="" loading="lazy"></iframe>
         </div>
     )
 }
