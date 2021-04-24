c11c81129a03562dd26f51ebb1ac0baee9fba26c
Aniket762 <aniketindian8@gmail.com>
2021-04-24 12:28:26 +0530

    members page

---
+++ src/components/table/table.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+    padding:'1%',
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function createData(Category, Subscription, Type) {
+  return { Category,Subscription,Type };
+}
+
+const rows = [
+    createData('Member', '₹ 600', 'Annually'),
+    createData('Student Member','₹ 300', 'Annually'),
+    createData('Life Member *below 60','₹ 6000', 'Lump Sum'),
+    createData('Life Member *60 or above','₹ 3000', 'Lump Sum'),
+    createData('Institutional Member', '₹ 6000', 'Annually'),
+    createData('Corporate Member', '₹ 60000', 'Annually'),
+    createData('Overseas Life Member','USD 1000', 'Lump Sum'),
+    createData('Overseas Annual Member', 'USD 100', 'Annually'),
+    createData('Overseas Institutional Member','USD 1000','Annually'),
+  
+];
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 700,
+  },
+});
+
+export default function CustomizedTables() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Category</StyledTableCell>
+            <StyledTableCell align="right">Subscription</StyledTableCell>
+            <StyledTableCell align="right">Type</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.Category}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.Subscription}</StyledTableCell>
+              <StyledTableCell align="right">{row.Type}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/pages/members.jsx
+++ src/pages/members.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import styled from "styled-components";
+import Typography from '@material-ui/core/Typography';
+import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
 
 import Card from '../components/card/card';
+import InfoSection from '../components/InfoSection/InfoSection';
+import Table from '../components/table/table';
+
+
+const theme = createMuiTheme({
+  typography: {
+    h3 :{
+      fontSize: '30px',
+      fontWeight:'bold',
+      padding:'2%',
+      '@media (max-width:800px)': {
+        fontSize: '2rem',
+      },
+    }
+  },
+});
 
 const teamData = [
     {
@@ -27,6 +46,21 @@ const teamData = [
     
 ]
 
+const durgapurChapter = {
+  primary: true,
+  lightBg: true,
+  lightTopLine: true,
+  lightText: false,
+  lightTextDesc: false,
+  headline: 'About Durgapur Chapter',
+  description:
+    'ORSI Durgapur is a very active chapter. There are more than 55 members in this chapter. Over the year many seminars, conferences, workshops are being organized. Mr. Salil Kumar Dutta & Dr. Kajla Basu served as president & vice president of the chapter for long time.There are many more active members of the society who are actively taking part to organize various program jointly with other organizations.',
+  buttonLabel: 'Membership Form',
+  imgStart: '',
+  img: require('../assets/images/orsi-durgapur.jpeg').default,
+  alt: 'postman api',
+  start: ''
+};
 
 function createCard(teamData)
 {
@@ -48,11 +82,19 @@ function members() {
    
   return (
     <div className='App'>
+      <InfoSection {...durgapurChapter} />
       <h1>TEAM</h1>
       <Grid lg={12} item container spacing={3} align='center'>
         {teamData.map(createCard)}
       </Grid>
+
+      <ThemeProvider theme={theme}>
+        <Typography variant="h3">Membership Subscription</Typography>
+      </ThemeProvider>
+      <Table />
+
     </div>
+   
   );
 }
 
