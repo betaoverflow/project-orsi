ec2480333dab77a9f8fbe592ea8a6fc542b9e1ea
Wolfgang-stack <wolfgang.a.mcrae@gmail.com>
2021-04-25 22:36:45 -0400

    modified and added styles for the contact card components
    changed color of icons
    
    changed alignment and size of email links
    
    added wrapping div element for card subtitles
    
    changed h1 elements to h2

--- src/components/contactCardGrid/contactCard.jsx
+++ src/components/contactCardGrid/contactCard.jsx
@@ -6,15 +6,22 @@ const useStyles = makeStyles({
     background: "rgb(152, 190, 231, 0.15)",
     borderRadius: "50%",
     padding: "30px",
-    color: "rgb(65, 105, 225)",
-    margin: "0 auto",
+    color: "rgb(0, 0, 246)",
     border: "1px solid rgb(152, 190, 231, 0.5)",
   },
   card: {
     height: "100%",
     width: "90%",
     // border: "1px solid grey", not sure if it looks better with a border or not
-    boxShadow: "3px 3px 10px 3px rgb(240,240,240)",
+    boxShadow: "3px 3px 10px 3px rgb(230,230,230)",
+    border: '0.5px solid rgb(230,230,230)',
+    borderRadius: '10px',
+    margin: '0 auto',
+  },
+  cardContent: {
+    margin: '0 auto',
+    padding: '10%',
+    textAlign: 'left',
   },
   subtitle: {
     marginBottom: 0,
@@ -22,26 +29,36 @@ const useStyles = makeStyles({
     lineHeight: 1,
     color: "grey",
   },
+  link: {
+    marginBlockStart: '16px',
+    lineHeight: 1,
+    fontSize: '16px',
+    lineHeight: 1,
+  }
 });
 
 const ContactCard = (props) => {
   const classes = useStyles();
   return (
     <Card className={classes.card}>
-      <CardContent>
+      <CardContent className={classes.cardContent}>
         <Avatar className={classes.avatar}>{props.icon}</Avatar>
-        <h1>{props.title}</h1>
+        <h2 style={{marginBottom: 0}}>{props.title}</h2>
         {/* map through the subtitles array
         if 'subtitle' is an email (includes '@') it will display as a link */}
+        <div>
         {props.subtitles.map((subtitle) =>
           subtitle.includes("@") ? (
-            <Link display="block" href={"mailto:" + subtitle} target="_blank">
+            <Link className={classes.link} display="block" href={"mailto:" + subtitle} target="_blank">
               {subtitle}
             </Link>
           ) : (
             <p className={classes.subtitle}>{subtitle}</p>
+
           )
+          
         )}
+        </div>
       </CardContent>
     </Card>
   );
--- src/components/contactCardGrid/contactCardGrid.jsx
+++ src/components/contactCardGrid/contactCardGrid.jsx
@@ -5,15 +5,21 @@ import ContactCard from "./contactCard";
 const ContactCardGrid = () => {
   return (
     <Grid
-      container
-      spacing={4}
       justify="center"
-      style={{ margin: "0 auto 20px auto" }}
+      container
+      spacing={2}
+      style={{ margin: "20px auto 80px auto" }}
     >
       {/* map through the contact data objects
     pass through data points as props */}
       {contactData.map((data) => (
-        <Grid item xs={11} sm={5} lg={3}>
+        <Grid
+          item
+          xs={11}
+          sm={8}
+          lg={4}
+          style={{ maxWidth: "450px", minWidth: "300px", width: "30%" }}
+        >
           <ContactCard
             icon={data.icon}
             title={data.title}
